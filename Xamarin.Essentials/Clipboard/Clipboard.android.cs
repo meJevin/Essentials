7ab483a3e48554aec0e8ed5de62a7d20fb223746
Michael <mejevin@gmail.com>
2021-04-03 06:18:34 +0300

    feat: Clipboard.SetImageAsync for Android

--- Xamarin.Essentials/Clipboard/Clipboard.android.cs
+++ Xamarin.Essentials/Clipboard/Clipboard.android.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Android.App;
 using Android.Content;
+using Android.Graphics;
+using Android.Provider;
+using Android.Util;
 using static Android.Content.ClipboardManager;
 
 namespace Xamarin.Essentials
@@ -9,6 +14,29 @@ namespace Xamarin.Essentials
     {
         static readonly Lazy<ClipboardChangeListener> clipboardListener
             = new Lazy<ClipboardChangeListener>(() => new ClipboardChangeListener());
+
+        static Task PlatformSetImageAsync(string b64Img)
+        {
+            var imageAsBytes = Base64.Decode(b64Img, Base64Flags.Default);
+            var bmp = BitmapFactory.DecodeByteArray(imageAsBytes, 0, imageAsBytes.Length);
+
+            var sdCardPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var filePath = System.IO.Path.Combine(sdCardPath, "temp-xe-clipboard-img.png");
+
+            using var stream = new FileStream(filePath, FileMode.Create);
+            bmp.Compress(Bitmap.CompressFormat.Png, 100, stream);
+            stream.Close();
+
+            var contentValues = new ContentValues(2);
+            contentValues.Put("mime_type", "image/png");
+            contentValues.Put("_data", filePath);
+
+            var uri = Application.Context.ContentResolver.Insert(MediaStore.Images.Media.ExternalContentUri, contentValues);
+
+            Platform.ClipboardManager.PrimaryClip = ClipData.NewUri(Application.Context.ContentResolver, "XamarinImageToClipboard", uri);
+
+            return Task.CompletedTask;
+        }
 
         static Task PlatformSetTextAsync(string text)
         {
